5ba061cff996229b11bb7e0bce78216615e1e3fe
Krithik Singh <krithiksingh@gmail.com>
2023-05-02 18:22:56 -0400

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', (e) => {
+    // your code here
+    let form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        buildToDo(e.target.querySelector('#new-task-description').value);
+        form.reset();
+    });
 });
+
+function buildToDo(todo) {
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', handleDelete);
+    btn.textContent = 'x';
+    p.textContent = `${todo} `;
+    document.querySelector('#list').appendChild(p).appendChild(btn);
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove();
+}
